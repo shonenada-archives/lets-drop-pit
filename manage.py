6c67fc2b15cd185ec10c7be0e4c598431cf90f3d
shonenada <shonenada@gmail.com>
2014-04-16 18:26:29 +0800

    add a quick command to drop db

--- manage.py
+++ manage.py
@@ -8,5 +8,8 @@ from development import config
 application = create_app(config)
 
 if sys.argv[1] == 'db':
+    if len(sys.argv) > 2 and sys.argv[2] == '-d':
+        db.drop_all()
+        print 'Droped database.'
     db.create_all()
     print 'Create database.'
