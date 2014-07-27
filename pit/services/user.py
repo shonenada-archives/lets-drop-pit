ecca89881918873b3c3e92e454ba3ea0207cdbc9
shonenada <shonenada@gmail.com>
2014-07-27 23:48:38 +0800

    add sign in

--- pit/services/user.py
+++ pit/services/user.py
@@ -7,3 +7,16 @@ def check_email_exist(email):
         return True
     else:
         return False
+
+
+def authenticate(email, raw_passwd):
+    if not check_email_exist(email):
+        return None
+
+    hashed_passwd = User.hash_password(raw_passwd)
+    user = (User.query.filter_by(email=email).filter_by(password=hashed_passwd)
+            .first())
+    if user:
+        return user
+    else:
+        return None
--- pit/views/account.py
+++ pit/views/account.py
@@ -1,26 +1,40 @@
-from tornado.web import RequestHandler
-
+from pit.views.base import View
 from pit.app import db
 from pit.models import User
-from pit.forms import SignUpForm
+from pit.forms import SignUpForm, SignInForm
+from pit.services.user import authenticate
 
 
-class SignUpView(RequestHandler):
+class SignUpView(View):
 
     def post(self):
         form = SignUpForm(self.request.arguments)
+
         if not form.validate():
             return self.finish({'success': False, 'messages': form.errors})
-        new_user = User(email=email, nickname=nickname)
-        new_user.set_password(password)
+
+        new_user = User(email=form.data['email'],
+                        nickname=form.data['nickname'])
+        new_user.set_password(form.data['password'])
         db.session.add(new_user)
         db.session.commit()
         return self.finish({'success': True,
                            'message': 'Register Successfully.'})
 
 
-class SignInView(RequestHandler):
+class SignInView(View):
 
     def post(self):
-        email = self.get_argument('email')
-        print email
+        form = SignInForm(self.request.arguments)
+
+        if not form.validate():
+            return self.finish({'success': False, 'messages': form.errors})
+
+        user = authenticate(form.data['email'], form.data['raw_passwd'])
+
+        if not user:
+            return self.finish({'success': False,
+                                'messages': 'Wrong username or password.'})
+        else:
+            # login user
+            self.set_secure_cookie('user_id', user.id)
